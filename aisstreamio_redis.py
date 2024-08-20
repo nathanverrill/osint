46658cdfa53a15c9b421c115fb9b8c64b37ef87a
nathanverrill <nathanverrill@gmail.com>
2024-08-20 15:05:02 -0500

    wip

--- aisstreamio_redis.py
+++ aisstreamio_redis.py
@@ -1,40 +1,43 @@
-import base64
-import json
+# see https://docs.bytewax.io/stable/tutorials/orderbook-guide/index.html
+import orjson as json
+from dataclasses import dataclass, field
+from datetime import timedelta
+from typing import Dict, List, Optional
 
+import websockets
+from bytewax import operators as op
+from bytewax.connectors.stdio import StdOutSink
 from bytewax.dataflow import Dataflow
-from bytewax.inputs import ManualInputConfig, distribute
+from bytewax.inputs import FixedPartitionedSource, StatefulSourcePartition, batch_async
 
 
-from websocket import create_connection
+url = "wss://stream.aisstream.io/v0/stream"
+key = "23f01fb154ef06c103887b7518f1482bc4b0f25d"
+box = ["[[-90, -180], [90, 180]]"]
 
-## input
-ticker_list = ['AMZN', 'MSFT']
+async def _ws_agen():
+    """Connect to websocket and yield messages as they arrive.
 
-def aisstreamio_input(worker_tickers, state):
-        ws = create_connection("wss://streamer.finance.yahoo.com/")
-        ws_subscription = {
-            "APIKey": api_key,
-            "BoundingBoxes": bounding_boxes,            
-        }
-        ws.send(json.dumps({"subscribe": worker_tickers}))
+    This function is a generator that connects to the  websocket and
+    yields messages as they arrive. 
+
+    Yields:
+        _type_: A tuple of the product_id and the message as a dictionary.
+    """
+
+    async with websockets.connect(url) as websocket:
+        msg = json.dumps(
+            {
+            "APIKey": key,
+            "BoundingBoxes": box,
+            }
+        )
+        await websocket.send(msg)
+        # The first msg is just a confirmation that we have subscribed.
+        await websocket.recv()
+
         while True:
-            yield state, ws.recv()
+            msg = await websocket.recv()
+            yield (json.loads(msg))
             
-with websockets.connect("wss://stream.aisstream.io/v0/stream", ping_interval=30, ping_timeout=30) as websocket:
-        # Subscribe to the AIS stream
-        subscribe_message = {
-            "APIKey": api_key,
-            "BoundingBoxes": bounding_boxes,
-        }
-        await websocket.send(orjson.dumps(subscribe_message))            
-
-
-def input_builder(worker_index, worker_count, resume_state):
-    state = resume_state or None
-    worker_tickers = list(distribute(ticker_list, worker_index, worker_count))
-    print({"subscribing to": worker_tickers})
-    return aisstreamio_input(worker_tickers, state)
-
-
-flow = Dataflow()
-flow.input("input", ManualInputConfig(input_builder))
+    
